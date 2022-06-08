7a1fef6df2df06ae83274c5c56a9324d6ae90d26
RedlineLW <74669373+RedlineLW@users.noreply.github.com>
2022-06-09 02:16:51 +0500

    Add files via upload

---
+++ AdminForm.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace zCalcMoney
+{
+    partial class AdminForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminForm));
+            this.metroLabel7 = new MetroFramework.Controls.MetroLabel();
+            this.buhButton = new MetroFramework.Controls.MetroButton();
+            this.kadrButton = new MetroFramework.Controls.MetroButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroLabel7
+            // 
+            this.metroLabel7.Location = new System.Drawing.Point(620, 7);
+            this.metroLabel7.Name = "metroLabel7";
+            this.metroLabel7.Size = new System.Drawing.Size(100, 23);
+            this.metroLabel7.TabIndex = 22;
+            // 
+            // buhButton
+            // 
+            this.buhButton.Location = new System.Drawing.Point(149, 100);
+            this.buhButton.Name = "buhButton";
+            this.buhButton.Size = new System.Drawing.Size(225, 30);
+            this.buhButton.TabIndex = 23;
+            this.buhButton.Text = "Перейти к окну бухгалтера";
+            this.buhButton.UseSelectable = true;
+            this.buhButton.Click += new System.EventHandler(this.buhButton_Click);
+            // 
+            // kadrButton
+            // 
+            this.kadrButton.Location = new System.Drawing.Point(380, 100);
+            this.kadrButton.Name = "kadrButton";
+            this.kadrButton.Size = new System.Drawing.Size(225, 30);
+            this.kadrButton.TabIndex = 24;
+            this.kadrButton.Text = "Перейди к окну кадров";
+            this.kadrButton.UseSelectable = true;
+            this.kadrButton.Click += new System.EventHandler(this.kadrButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(653, 243);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(67, 36);
+            this.pictureBox1.TabIndex = 26;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(264, 155);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(456, 22);
+            this.textBox1.TabIndex = 27;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(26, 243);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(200, 30);
+            this.metroButton1.TabIndex = 29;
+            this.metroButton1.Text = "Добавить пользователя";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(53, 157);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(165, 20);
+            this.metroLabel1.TabIndex = 30;
+            this.metroLabel1.Text = "Фамилия Имя Отчество";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(55, 183);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(163, 20);
+            this.metroLabel2.TabIndex = 31;
+            this.metroLabel2.Text = "Занимаемая должность";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(264, 183);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(456, 22);
+            this.textBox2.TabIndex = 32;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(264, 211);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(456, 22);
+            this.textBox3.TabIndex = 34;
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(108, 213);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(48, 20);
+            this.metroLabel3.TabIndex = 33;
+            this.metroLabel3.Text = "Логин";
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(438, 243);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(200, 30);
+            this.metroButton2.TabIndex = 35;
+            this.metroButton2.Text = "Удалить пользователя";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Location = new System.Drawing.Point(232, 243);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(200, 30);
+            this.metroButton3.TabIndex = 36;
+            this.metroButton3.Text = "Список пользователей";
+            this.metroButton3.UseSelectable = true;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // AdminForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 307);
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.kadrButton);
+            this.Controls.Add(this.buhButton);
+            this.Controls.Add(this.metroLabel7);
+            this.Name = "AdminForm";
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Red;
+            this.Text = "Администрирование";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.AdminForm_FormClosed);
+            this.Load += new System.EventHandler(this.AdminForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLabel metroLabel7;
+        private MetroFramework.Controls.MetroButton buhButton;
+        private MetroFramework.Controls.MetroButton kadrButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton3;
+    }
+}
---
+++ AdminForm.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace zCalcMoney
+{
+    public partial class AdminForm : MetroFramework.Forms.MetroForm
+    {
+        public AdminForm()
+        {
+            InitializeComponent();
+        }
+
+        string conn = "server = chuc.caseum.ru; port = 33333; user = st_2_18_23; database = is_2_18_st23_VKR; password = 51922291;";
+        string fio;
+        string role1;
+        string login;
+        string password;
+        Random rnd = new Random();
+
+        private void AdminForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buhButton_Click(object sender, EventArgs e)
+        {
+            MainForm brig = new MainForm();
+            brig.Owner = this;
+            brig.ShowDialog();
+        } // переход к окну бухгалтера
+
+        private void kadrButton_Click(object sender, EventArgs e)
+        {
+            kadrForm kadr = new kadrForm();
+            kadr.Owner = this;
+            kadr.ShowDialog();
+        } // переход к окну кадров
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            fio = textBox1.Text;
+            role1 = textBox2.Text;
+            login = textBox3.Text;
+
+            Password P = new Password();
+            password = P.GetPass();
+
+            MySqlConnection mysql_connection = new MySqlConnection(conn);
+            MySqlCommand mysql_query = mysql_connection.CreateCommand();
+            mysql_query.CommandText = "INSERT INTO users (fio, role, login, password) " +
+                "VALUES ('" + fio + "', '" + role1 + "', '" + login + "', '" + password + "');";
+            mysql_connection.Open();
+            MySqlDataReader mysql_result;
+            mysql_result = mysql_query.ExecuteReader();
+
+            MessageBox.Show("Пользователь " + fio + " зарегестрирован с должностью: " + role1 + " ; Логином " + login + " ; Паролем " + password + " ");
+            
+        } // зарегистрировать нового пользователя
+
+        private void AdminForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            login = textBox3.Text;
+            MySqlConnection mysql_connection = new MySqlConnection(conn);
+            MySqlCommand mysql_query = mysql_connection.CreateCommand();
+            mysql_query.CommandText = "SELECT fio FROM users WHERE login = '" + login + "';";
+            mysql_connection.Open();
+            MySqlDataReader mysql_result;
+            mysql_result = mysql_query.ExecuteReader();
+            while (mysql_result.Read()) // вытаскиваем из БД нынешние коэффициенты
+            {
+                fio = mysql_result[0].ToString();
+            }
+            mysql_result.Close();
+            mysql_connection.Close();
+            DialogResult result = MessageBox.Show("Вы действительно хотите удалить пользователя: " + fio + " ",
+        "Сообщение",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1);
+            if (result == DialogResult.Yes)
+            {
+                mysql_query.CommandText = "DELETE FROM users WHERE login = '" + login + "';";
+                mysql_connection.Open();
+                mysql_result = mysql_query.ExecuteReader();
+                //Вызов метода обновления ДатаГрида            
+                this.Refresh();
+                mysql_result.Close();
+                mysql_connection.Close();
+                MessageBox.Show("Пользователь " + fio + " удалён");
+            }
+        } // Удалить пользователя
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            string list = "";
+            MySqlConnection mysql_connection = new MySqlConnection(conn);
+            MySqlCommand mysql_query = mysql_connection.CreateCommand();
+            mysql_query.CommandText = "SELECT * FROM users;";
+            mysql_connection.Open();
+            MySqlDataReader mysql_result;
+            mysql_result = mysql_query.ExecuteReader();
+            while (mysql_result.Read()) {
+                list += "Пользователь: " + mysql_result[1].ToString() + " Должность: " + mysql_result[2].ToString() + " Логин: " + mysql_result[3].ToString() + "\n";
+            }
+            MessageBox.Show(list, "Cписок пользователей");
+        }
+    }
+} 
